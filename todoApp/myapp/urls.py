30ebd8fe1a22e60b7a18eb108a182a5467636cc4
unknown <subhambanerjee138@gmail.com>
2020-04-11 13:35:31 +0530

    Finalizing the project and working commit

--- todoApp/myapp/urls.py
+++ todoApp/myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('',views.index, name='index'),
+	path('',views.index, name ='index'),
+	path('addTask',views.addTask, name = 'addTask'),
+	path('delete/<int:todo_id>/',views.delete, name = 'delete')
 ]
--- todoApp/myapp/views.py
+++ todoApp/myapp/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, redirect
+from django.utils import timezone
+from .models import ToDo
 
-# Create your views here.
 def index(request):
+	tasks = ToDo.objects.all().order_by("-time")
 
-	return render(request, "index.html")
+	return render(request, "index.html",{'tasks' : tasks})
+
+def addTask(request):
+	if request.method == 'POST':
+		curr = timezone.now()
+		task = request.POST['task']
+		print(task)
+		obj = ToDo.objects.create(task = task, time = curr)
+		obj.save()
+	return redirect('/')	
+
+def delete(request, todo_id):
+	ToDo.objects.get(id=todo_id).delete()
+	return redirect('/')
